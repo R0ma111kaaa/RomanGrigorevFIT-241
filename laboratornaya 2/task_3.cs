a637a85c3123fe59b36de78819456b1f91ae3e77
R0ma111kaaa <150804973+R0ma111kaaa@users.noreply.github.com>
2024-09-26 10:50:46 +0600

    laba 2

---
+++ laboratornaya 2/task_3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Введите количество элементов: ");
+        int quan = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Введите {quan} элементов: ");
+        int max1 = int.Parse(Console.ReadLine());
+        int max2 = int.Parse(Console.ReadLine());
+        
+        if (max1 < max2) {
+            int temp = max1;
+            max1 = max2;
+            max2 = temp;
+        }
+        
+        for (int i = 0; i < quan - 2; i++) {
+            int element = int.Parse(Console.ReadLine());
+            if (element > max1) {
+                max2 = max1;
+                max1 = element;
+            } else if (element > max2) {
+                max2 = element;
+            }
+        }
+        Console.WriteLine($"Максимумы: {max1}, {max2};");
+    }
+}
