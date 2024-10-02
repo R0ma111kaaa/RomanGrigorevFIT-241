c7e0859e165f7592b0649d6930a1e9ff84cebac5
R0ma111kaaa <150804973+R0ma111kaaa@users.noreply.github.com>
2024-10-02 17:34:51 +0600

    Add files via upload

---
+++ laboratornaya.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество элементов последовательности: ");
+        int n = int.Parse(Console.ReadLine());
+        // task 1
+        int maxCountSameElems = 1, currentCountSameElems = 1;
+        // task 2
+        int minCountEvenElems = int.MaxValue, currentCountEvenElems;
+        // task 3
+        int maxSumEvenElems, currentSumEvenElems;
+
+
+        int num;
+        Console.WriteLine("Вводите элементы через Enter: ");
+        int previous = int.Parse(Console.ReadLine());
+        currentCountEvenElems = previous % 2 == 0 ? 1 : 0;
+        maxSumEvenElems = currentSumEvenElems = previous % 2 == 0? previous : 0;
+        for (int i = 1; i < n; i++)
+        {
+            num = int.Parse(Console.ReadLine());
+            // 1 задача
+
+            if (num == previous) {
+                currentCountSameElems++;
+            } else {
+                currentCountSameElems = 1;
+            }
+            if (currentCountSameElems > maxCountSameElems) {
+                maxCountSameElems = currentCountSameElems;
+            }
+            // 2 задача
+            if (num % 2 == 0) {
+                currentCountEvenElems++;
+            } else {
+                if (currentCountEvenElems > 0 && currentCountEvenElems < minCountEvenElems) {
+                    minCountEvenElems = currentCountEvenElems;
+                }
+                currentCountEvenElems = 0;
+            }
+            // 3 задача
+            if (num % 2 == 0) {
+                currentSumEvenElems += num;
+            } else {
+                currentSumEvenElems = 0;
+            }
+            if (maxSumEvenElems < currentSumEvenElems) {
+                    maxSumEvenElems = currentSumEvenElems;
+            }
+
+            // finally
+            previous = num;
+        }
+        Console.WriteLine($"ответ на задание 1: {maxCountSameElems} (максимальный размер последовательности из одинаковых элементов)");
+        Console.WriteLine($"ответ на задание 2: {(minCountEvenElems == int.MaxValue ? 0 : minCountEvenElems)} (минимальный размер последовательности из четных)");
+        Console.WriteLine($"ответ на задание 3: {maxSumEvenElems} (макс сумма последовательности из четных)");
+
+    }
+}
