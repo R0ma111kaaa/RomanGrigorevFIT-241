f292fe245332c3f7d79e4af7f664e9295c8e35c4
roma111kaaa <roma111kaaa@mail.ru>
2024-12-30 01:42:55 +0600

    full rgr

---
+++ rgr/krestyanin and chertila/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+class MainClass
+{
+    static void Main()
+    {
+        int totalCount = 0;
+        const int upperLimit = 1000;
+
+        for (int number = 1; number <= upperLimit; number++)
+        {
+            for (int step = 2; step <= number * 1.34; step += 2)
+            {
+                int tempValue = number;
+
+                for (int iteration = 1; iteration <= 20; iteration++)
+                {
+                    tempValue = tempValue * 2 - step;
+
+                    if (tempValue == 0)
+                    {
+                        totalCount++;
+                        break;
+                    }
+
+                    if (tempValue < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(totalCount + upperLimit);
+    }
+}
