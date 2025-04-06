4387a3c0f4b52d4eafe0d667c7a6a1d164b0e77a
Evgeny Bazarov <roma111kaaa@mail.ru>
2025-04-06 21:54:32 +0600

    2 и 3 лабы

---
+++ 2_sem/DM/2_laba/PrimAlgoritm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+// РАБОЧИЙ ПРИМЕР
+
+// Введите количество узлов в графе:
+// 7
+// Введите весовую матрицу отношений
+// строки отделяются пробелом, столбцы отеделяются энтером
+// 0 2 0 6 0 0 0  
+// 2 0 3 8 5 0 0  
+// 0 3 0 0 7 0 0
+// 6 8 0 0 9 0 0  
+// 0 5 7 9 0 1 2
+// 0 0 0 0 1 0 4
+// 0 0 0 0 2 4 0 
+// 
+// 19
+
+
+
+
+namespace vivid
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество узлов в графе:");
+            int nodeCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите весовую матрицу отношений");
+            Console.WriteLine("строки отделяются пробелом, столбцы отеделяются энтером");
+
+            int[][] matrix = new int[nodeCount][];
+            for (int i = 0; i < nodeCount; i++)
+            {
+                string line = Console.ReadLine();
+                matrix[i] = line.Trim().Split(' ').Select(int.Parse).ToArray();
+            }
+
+            List<int> usedNodes = new List<int> {0};
+
+            int resultSum = 0;
+            while (usedNodes.Count != nodeCount) {
+                int minPath = int.MaxValue;
+                int minNode = -1;
+                foreach (int i in usedNodes) {
+                    for (int  j= 0; j < nodeCount; j++) {
+                        if (!usedNodes.Contains(j)) {
+                            int currentPath = matrix[i][j];
+                            if (currentPath < minPath && currentPath > 0) {
+                                minPath = currentPath;
+                                minNode = j;
+                            }
+                        }
+                    }
+                }
+                usedNodes.Add(minNode);
+                resultSum += minPath;
+            }
+            Console.WriteLine(resultSum);
+        }
+    }
+}
---
+++ 2_sem/DM/3_laba/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+// Задание: найти мосты по остовному дереву.
+// По сути то же самое, что и 2 задание, но в ответе список ребер
+
+
+
+
+namespace vivid
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество узлов в графе:");
+            int nodeCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите весовую матрицу отношений");
+            Console.WriteLine("строки отделяются пробелом, столбцы отеделяются энтером");
+
+            int counter = 0;
+            var edgeList = new (int, int, int)[nodeCount * nodeCount];
+            for (int i = 0; i < nodeCount; i++)
+            {
+                string input = Console.ReadLine();
+                int[] currentLine = input.Trim().Split(' ').Select(int.Parse).ToArray();
+                for (int j = i; j < nodeCount; j++) {
+                    int currentElement = currentLine[j];
+                    if (currentElement != 0) {
+                        edgeList[counter] = (i, j, currentElement);
+                        counter++;
+                    }
+                }
+            }
+
+            int[] parent = new int[nodeCount];
+            for (int i = 0; i < nodeCount; i++) {
+                parent[i] = i;
+            }
+
+            var mst = new System.Collections.Generic.List<(int, int, int)>();
+            int sum = 0;
+            foreach (var edge in edgeList.OrderBy(edge => edge.Item3).ToArray()) {
+                int u = edge.Item1;
+                int v = edge.Item2;
+                int cost = edge.Item3;
+
+                if (findParent(u) != findParent(v)) {
+                    mst.Add(edge);
+                    Console.WriteLine($"{u}-{v}: {cost}");
+                    sum += cost;
+                    union(u, v);
+                }
+            }
+            Console.WriteLine(sum);
+
+            int findParent(int x) {
+                if (parent[x] != x) {
+                    parent[x] = findParent(parent[x]);
+                }
+                return parent[x];
+            }
+  
+            void union(int x, int y) {
+                int py = findParent(y);
+                int px = findParent(x);
+                parent[py] = px;
+            }
+        }
+    }
+}
