fb3f54f097a9ebcdd2a2b892bd14bf96f9809317
Evgeny Bazarov <roma111kaaa@mail.ru>
2025-04-16 14:16:21 +0600

    all labs aip

---
+++ 2_sem/AIP/8_laba/lambda_calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<double, double, double> sum = (x, y) => x + y;
+        Func<double, double, double> difference = (x, y) => x - y;
+        Func<double, double, double> product = (x, y) => x * y;
+        Func<double, double, double> quotient = (x, y) =>
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Невозможно разделить на ноль");
+            }
+            return x / y;
+        };
+
+        double num1 = 10;
+        double num2 = 0;
+
+        Console.WriteLine($"Результат сложения: {sum(num1, num2)}");
+        Console.WriteLine($"Результат вычитания: {difference(num1, num2)}");
+        Console.WriteLine($"Результат умножения: {product(num1, num2)}");
+
+        try
+        {
+            Console.WriteLine($"Результат деления: {quotient(num1, num2)}");
+        }
+        catch (DivideByZeroException error)
+        {
+            Console.WriteLine(error.Message);
+        }
+    }
+}
---
+++ 2_sem/AIP/8_laba/lambda_string/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var list = new List<string> 
+        {
+            "мир", "привет", "молоко", "яблоко", "машина", "книга", "медведь"
+        };
+
+        var selectedWords = list.Where(item => item.StartsWith("м")).ToList();
+
+        Console.WriteLine("Слова с буквы 'м':");
+        selectedWords.ForEach(Console.WriteLine);
+    }
+}
