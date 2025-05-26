230ed986945007736ce8e4336e71567250de7d9d
Evgeny Bazarov <roma111kaaa@mail.ru>
2025-05-26 10:18:30 +0600

    11 laba aip etc

---
+++ 2_sem/AIP/09_laba/Program.cs
@@ -0,0 +1,46 @@
+﻿class TypedArray<T>
+{
+    private T[] items;
+    private int count;
+
+    public TypedArray(int capasity = 10)
+    {
+        items = new T[capasity];
+        count = 0;
+    }
+
+    public int Count => count;
+
+    public void Add(T item)
+    {
+        if (count >= items.Length)
+        {
+            Array.Resize<T>(ref items, items.Length * 2);
+        }
+        items[count] = item;
+        count++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || items.Length <= index)
+        
+            throw new IndexOutOfRangeException("Индекс за границей");
+        
+        for (int i = index; i < items.Length - 1; i++)
+        {
+            items[i] = items[i + 1];
+        }
+        count--;
+        items[count] = default!;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || items.Length <= index)
+        
+            throw new IndexOutOfRangeException("Индекс за границей");
+        
+        return items[index];
+    }
+}
---
+++ 2_sem/AIP/11_laba/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main()
+    {
+        string inputPath = "input.txt";
+        string outputPath = "output.txt";
+        if (!File.Exists(inputPath))
+        {
+            throw new Exception("файла нету");
+        }
+
+        string[] lines = File.ReadAllLines(inputPath);
+
+        using (StreamWriter writer = new StreamWriter(outputPath))
+        {
+            foreach (string line in lines)
+            {
+                if (hasNum(line))
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        bool hasNum(string line)
+        {
+            string oddNums = "02468";
+            bool isLastOdd = false;
+            foreach (char ch in line)
+            {
+                if (oddNums.Contains(ch))
+                {
+                    isLastOdd = true;
+                }
+                else
+                {
+                    if (isLastOdd)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        isLastOdd = false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
